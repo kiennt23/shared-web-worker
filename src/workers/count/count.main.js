bb12467c52b085b53033811d639aab5c07a564b5
Kien Nguyen <kien@xendit.co>
2022-10-25 22:53:56 +0700

    Add comments to the worker in main thread code

--- src/workers/count/count.main.js
+++ src/workers/count/count.main.js
@@ -8,14 +8,23 @@ const worker = initWorker();
 
 let handlers = [];
 
+/**
+* close the worker/connection by sending a "close" command to the worker
+*/
 export const closeWorker = () => {
     worker.port.postMessage({ command: "close" });
 }
 
+/**
+* Register a count handler
+*/
 export const registerCountHandlers = (handler) => {
     handlers.push(handler);
 }
 
+/**
+* handle the data sent from the worker by calling each hander in the `handlers` list
+*/
 const handleMessage = () => {
     worker.port.onmessage = (event) => {
         handlers.forEach(handler => handler(event.data.counter));
